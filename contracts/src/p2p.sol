66df033fdb376027ebb4051a071c20d0f307a462
d33p <q5tdbj28@duck.com>
2025-10-16 13:10:08 +0700

    market based order system

--- contracts/src/p2p.sol
+++ contracts/src/p2p.sol
@@ -5,24 +5,30 @@ import {IPyth} from "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract P2P {
-    enum OrderStatus {
-        Open,
-        Filled,
-        Cancelled
+    event MarketCreated(bytes32 markteId, address tokenSell, address tokenBuy);
+
+    struct QueueNode {
+        uint256 nextId; //0 if this is the last
+        uint256 prevId; //0 if this is the first
     }
 
     struct Order {
-        uint256 id;
         address maker;
-        address tokenSell;
-        address tokenBuy;
         uint256 amountSell;
-        uint256 amountBuy; //for fixed price orders
-        bool allowPartial; //allow partial fills
-        OrderStatus status;
     }
 
-    mapping(uint256 => Order) public orders; //mapping orders by orderID
+    struct Market {
+        address tokenSell;
+        address tokenBuy;
+        mapping(uint256 => Order) orders; //order mapping
+        mapping(uint256 => QueueNode) queue; //linked list
+        uint256 headId; // first order
+        uint256 tailId; // last order
+        uint256 totalLiquidity;
+        uint256 nextOrderId;
+    }
+
+    mapping(bytes32 => Market) public markets; //mapping markets (keccak of tokenSell & tokenBuy)
     mapping(address => bytes32) public priceFeed; //mapping address to pricefeed Id;
 
     IPyth public pyth;
@@ -34,63 +40,36 @@ contract P2P {
         owner = msg.sender;
     }
 
+    function createMarket(address _tokenSell, address _tokenBuy) public {
+        require(
+            _tokenSell != address(0) && _tokenBuy != address(0),
+            "Invalid token address"
+        );
+        require(_tokenSell != _tokenBuy, "Tokens must be different");
+        require(priceFeed[_tokenSell] != bytes32(0));
+        require(priceFeed[_tokenBuy] != bytes32(0));
+        bytes32 marketId = keccak256(abi.encodePacked(_tokenSell, _tokenBuy));
+        require(
+            markets[marketId].tokenSell == address(0),
+            "Market already exists"
+        );
+        Market storage newMarket = markets[marketId];
+        newMarket.tokenSell = _tokenSell;
+        newMarket.tokenBuy = _tokenBuy;
+        newMarket.nextOrderId = 1;
+        emit MarketCreated(marketId, _tokenSell, _tokenBuy);
+    }
+
     function createOrder(
         address _tokenSell,
         address _tokenBuy,
-        uint256 _amountSell,
-        uint256 _amountBuy,
-        bool _allowPartial
-    ) public payable {
-        if (_amountBuy == 0) {
-            require(
-                priceFeed[_tokenSell] != bytes32(0),
-                "no price feed for the token you are tring to sell"
-            );
-            require(
-                priceFeed[_tokenBuy] != bytes32(0),
-                "no price feed for the token you are trying to buy"
-            );
-        }
-        if (_tokenSell == address(0)) {
-            require(msg.value == _amountSell);
-        }
-        if (_tokenSell != address(0)) {
-            IERC20(_tokenSell).transferFrom(
-                msg.sender,
-                address(this),
-                _amountSell
-            );
-        }
-
-        orders[orderIdCounter] = Order(
-            orderIdCounter,
-            msg.sender,
-            _tokenSell,
-            _tokenBuy,
-            _amountSell,
-            _amountBuy,
-            _allowPartial,
-            OrderStatus.Open
-        );
-        orderIdCounter++;
-    }
-
-    function cancelOrder(uint256 _orderId) public {
-        Order order = orders[_oderId];
+        uint256 _amountSell
+    ) public {
+        bytes32 marketId = keccak256(abi.encodePacked(_tokenSell, _tokenBuy));
         require(
-            order.maker == msg.sender,
-            "you can only cancel your own orders"
+            markets[marketId].tokenSell == address(0),
+            "no market for this exists"
         );
-        require(
-            order.status == OrderStatus.Open,
-            "Only open orders can be closed"
-        );
-
-        if(order.tokenSell == address(0)) {
-            (sent, data) = address(msg.sender).call{order[amountSell]}("");
-            require(sent, "Failed to sent ether")
-        }
-        orders[_orderId].status = OrderStatus.Cancelled;
     }
 
     function setPriceFeed(
