3adf64157c1806a674d9ceea910d8415aa2beae9
d33p <q5tdbj28@duck.com>
2025-10-25 23:54:00 +0700

    test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    }
+    return config
+  },
+  transpilePackages: ['@metamask/sdk'],
+}
 
 module.exports = nextConfig
